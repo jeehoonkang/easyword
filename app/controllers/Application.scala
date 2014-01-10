c2437eb7999de499875e9cccb4ca6a5eb3a6a96a
Jeehoon Kang <jhkang@ropas.snu.ac.kr>
2014-01-10 18:25:35 +0900

    Add submit feature.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,6 +15,7 @@ import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
+import play.api.Play.current
 import scala.concurrent.Future
 
 import reactivemongo.api._
@@ -23,35 +24,65 @@ import play.modules.reactivemongo.json.collection.JSONCollection
 
 import org.apache.commons.codec.binary.Base64
 
+import com.typesafe.plugin._
+
 object Application extends Controller with MongoController {
-	val ma = new MorphemeAnalyzer()
+  val ma = new MorphemeAnalyzer()
   val words: Set[String] = scala.io.Source.fromFile("public/words/words.txt").getLines.foldLeft(Set[String]())(_ + _)
 
+  def submissions: JSONCollection = db.collection[JSONCollection]("submissions")
   def collection: JSONCollection = db.collection[JSONCollection]("fails")
   
   def index = Action { implicit request =>
-    val text = request.queryString.get("text") match {
-      case None => ""
-      case Some(text) => text(0)
-    }
+    Ok(views.html.index())
+  }
 
-    Ok(views.html.index(text))
+  def submit = Action { implicit request =>
+    val author = request.body.asFormUrlEncoded.get("author")(0)
+    val content = request.body.asFormUrlEncoded.get("text")(0)
+    val contents = content.split("\\s")
+
+    if (contents.forall(spellcheckLookup(_))) {
+      val mail = use[MailerPlugin].email
+      mail.setSubject("Easyword (from " + author + ")")
+      mail.addRecipient("EasyWord <easyword@jeehoon.me>","easyword@jeehoon.me")
+      mail.addFrom("EasyWord <easyword@jeehoon.me>")
+      mail.send("author: " + author + "\ncontent:\n" + content)
+
+      submissions.insert(Json.obj("author" -> author, "content" -> content))
+
+      Ok(views.html.submit("제출되었어요!"))
+    }
+    else {
+      Ok(views.html.submit("어려운 단어가 있어서 제출 못했어요."))
+    }
   }
 
   def build = Action { implicit request =>
-    val filename = "public/words/fables-fails.txt"
-    val words = scala.io.Source.fromFile(filename).getLines
+    val filename = "public/words/fables.txt"
+    val lines = scala.io.Source.fromFile(filename).getLines.reduce(_ + " " + _)
+    val ws = lines.split("\\s")
     val out = new PrintWriter("public/words/fables-morpheme.txt")
-    for (word <- words) {
-      val result1 = ma.analyze(word)
-			val result2 = ma.postProcess(result1)
-			val result3 = ma.leaveJustBest(result2)
+    for (word <- ws) {
+      if (word != "") {
+        val result1 = ma.analyze(word)
+        if (result1 != null) {
+          val result2 = ma.postProcess(result1)
+          val result3 = ma.leaveJustBest(result2)
 
-      val sentence: Sentence = ma.divideToSentences(result3).asScala.toList.head
-      for (eojeol <- sentence) {
-        for (morpheme <- eojeol) {
-          out.println(morpheme.getString)
-          // out.println(morpheme.asInstanceOf[Token].getString())
+          for (sentence <- ma.divideToSentences(result3).asScala.toList) {
+            for (eojeol <- sentence) {
+              for (morpheme <- eojeol) {
+                if (
+                  morpheme.getTag()(0) != 'J' &&
+                    morpheme.getTag()(0) != 'E' &&
+                    morpheme.getTag()(0) != 'S' &&
+                    !words.contains(morpheme.asInstanceOf[Token].getString())) {
+                  out.println(morpheme.asInstanceOf[Token].getString())
+                }
+              }
+            }
+          }
         }
       }
     }
@@ -84,35 +115,40 @@ object Application extends Controller with MongoController {
   }
 
   def spellcheckLookup(text: String): Boolean = {
-		try {
-      val result1 = ma.analyze(text)
-			val result2 = ma.postProcess(result1)
-			val result3 = ma.leaveJustBest(result2)
+    if (text.length > 10000) return false
 
-      val sentences: List[Sentence] = ma.divideToSentences(result3).asScala.toList
-      for (sentence <- sentences) {
-				// out.append("=============================================<br />")
-        // out.append(sentence.getSentence() + "<br />")
-        for (word <- sentence) {
-          for (morpheme <- word) {
-            if (
-              !words.contains(morpheme.asInstanceOf[Token].getString()) &&
+    try {
+      if (text != "") {
+        val result1 = ma.analyze(text)
+        val result2 = ma.postProcess(result1)
+        val result3 = ma.leaveJustBest(result2)
+
+        val sentences: List[Sentence] = ma.divideToSentences(result3).asScala.toList
+        for (sentence <- sentences) {
+          // out.append("=============================================<br />")
+          // out.append(sentence.getSentence() + "<br />")
+          for (word <- sentence) {
+            for (morpheme <- word) {
+              if (
                 morpheme.getTag()(0) != 'J' &&
-                morpheme.getTag()(0) != 'E' &&
-                morpheme.getTag()(0) != 'S'
-            ) {
-              return false
-            }
+                  morpheme.getTag()(0) != 'E' &&
+                  morpheme.getTag()(0) != 'S' &&
+                  !words.contains(morpheme.asInstanceOf[Token].getString())
+              ) {
+                println("fail: " + morpheme.asInstanceOf[Token].getString())
+                return false
+              }
 
-            // morpheme.asInstanceOf[Token].getIndex()
-            // morpheme.asInstanceOf[Token].getString()
-            // morpheme.getTag()
+              // morpheme.asInstanceOf[Token].getIndex()
+              // morpheme.asInstanceOf[Token].getString()
+              // morpheme.getTag()
+            }
           }
         }
-			}
-		} catch {
-			case e: Exception => e.printStackTrace()
-		}
+      }
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
 
     return true
   }
--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   "org.webjars" %% "webjars-play" % "2.2.0", 
   "org.webjars" % "bootstrap" % "2.3.1",
-  "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2"
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2",
+  "com.typesafe" %% "play-plugins-mailer" % "2.1-RC2"
 )
 
 playScalaSettings
